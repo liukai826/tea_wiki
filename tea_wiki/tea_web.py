623039adc16a4e750ff3af04a9568558d0e9ffb0
kaka1024 <kaka1024@vip.qq.com>
2016-03-21 06:12:53 -0400

    modify templates

--- tea_wiki/tea_web.py
+++ tea_wiki/tea_web.py
@@ -1,14 +1,18 @@
 '''
     提供web后台管理界面
 '''
+from datetime import datetime
+
+from flask.ext.moment import Moment as moment
 from flask import render_template, request, abort
-from .models import User, News 
+from .models import User, News
 from . import app
 
 
 @app.route('/')
 def index():
-    return 'hello'
+    return render_template('index.html',current_time=datetime.utcnow())
+
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -17,4 +21,3 @@ def page_not_found(error):
     return render_template('errors.html',
                         title=title,
                         message=msg)
-
